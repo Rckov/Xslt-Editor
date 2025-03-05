d5fa89e40c4062039c191770b03e5a4eead5648d
Alexander Rychkov <Rychkov.dev@yandex.ru>
2025-03-05 11:10:25 +0300

    Edit styles

--- XsltEditor/ViewModels/Main/SettingsViewModel.cs
+++ XsltEditor/ViewModels/Main/SettingsViewModel.cs
@@ -1,7 +1,5 @@
 using System.Collections.ObjectModel;
-using System.Windows.Input;
 
-using XsltEditor.Infrastructure;
 using XsltEditor.Models.Base;
 using XsltEditor.Services;
 
@@ -9,101 +7,32 @@ namespace XsltEditor.ViewModels.Main;
 
 public class SettingsViewModel : ObservableObject
 {
-    private ThemeType _selectedTheme;
+    public SettingsViewModel()
+    {
+        FillThemes();
+    }
 
-    public ObservableCollection<ThemeType> Themes { get; } =
-    [
-        ThemeType.Light,
-        ThemeType.Dark
-    ];
+    public ObservableCollection<ThemeType> Themes { get; } = [];
+    public ObservableCollection<int> FontSize { get; } = [8, 10, 12, 14, 16, 18, 20];
 
     public ThemeType SelectedTheme
     {
-        get => _selectedTheme;
+        get => ThemeManager.CurrentTheme;
         set
         {
-            if (Set(ref _selectedTheme, value))
+            if (Set(ref field, value))
             {
                 ThemeManager.Apply(value);
             }
         }
     }
 
-    public Action? CloseWindow { get; set; }
-
-    public string? FontFamily
+    private void FillThemes()
     {
-        get;
-        set => Set(ref field, value);
+        foreach (ThemeType theme in Enum.GetValues(typeof(ThemeType)))
+        {
+            Themes.Add(theme);
+        }
     }
 
-    public int FontSize
-    {
-        get;
-        set => Set(ref field, value);
-    }
-
-    public bool ShowLineNumbers
-    {
-        get;
-        set => Set(ref field, value);
-    }
-
-    public bool ConvertTabsToSpaces
-    {
-        get;
-        set => Set(ref field, value);
-    }
-
-    public bool EnableEmailHyperlinks
-    {
-        get;
-        set => Set(ref field, value);
-    }
-
-    public bool EnableHyperlinks
-    {
-        get;
-        set => Set(ref field, value);
-    }
-
-    public bool EnableCodeFolding
-    {
-        get;
-        set => Set(ref field, value);
-    }
-
-    public int TabSize
-    {
-        get;
-        set => Set(ref field, value);
-    }
-
-    public int IndentSize
-    {
-        get;
-        set => Set(ref field, value);
-    }
-
-    public ICommand SaveCommand { get; }
-    public ICommand CancelCommand { get; }
-
-    public SettingsViewModel()
-    {
-        _selectedTheme = ThemeManager.CurrentTheme;
-
-        SaveCommand = new RelayCommand(Save);
-        CancelCommand = new RelayCommand(Cancel);
-    }
-
-    private void Save(object? parameter)
-    {
-        // TODO: Save settings
-        CloseWindow?.Invoke();
-    }
-
-    private void Cancel(object? parameter)
-    {
-        CloseWindow?.Invoke();
-    }
 }
