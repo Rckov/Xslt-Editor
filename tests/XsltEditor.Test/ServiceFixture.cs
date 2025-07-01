fa88370720038466b8bba87ad34f1aa5f4a3e486
Rckov <Rychkov.dev@yandex.ru>
2025-07-01 23:42:15 +0300

    chore: add xUnit test project

---
+++ tests/XsltEditor.Test/ServiceFixture.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XsltEditor.Test;
+internal class ServiceFixture
+{
+    public ServiceFixture()
+    {
+        Services = new ServiceCollection();
+
+        // add services
+    }
+
+    public IServiceCollection Services { get; set; }
+}
